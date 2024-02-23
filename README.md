a9615f7c0e5bb47622fe4acb776cec1a32be63f4
Jonathan Suru <jonathansuru@icloud.com>
2024-02-23 20:35:23 +0100

    📚 docs: add resources for RAG chatbot implementation

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 - [ ] Each chatbot has unlimited memory capacity, enabling seamless interaction with large files
 - [ ] Dockerize the project for easy deployment
 - [ ] Support vector database to provide chatbots with larger memory
+ 
+ 
+## Resources
+ 
+- https://huggingface.co/learn/cookbook/rag_evaluation
+- https://dspy-docs.vercel.app/docs/tutorials/rag
---
+++ dialogue/llm_provider.py
@@ -0,0 +1,38 @@
+import ollama
+
+
+class LLMProvider:
+    def __init__(self, prompt):
+        self.prompt = prompt
+
+    def query(self, prompt):
+        raise NotImplementedError
+
+
+class OpenAI(LLMProvider):
+    def query(self, prompt):
+        return "OpenAI"
+
+
+class Replicate(LLMProvider):
+    def query(self, prompt):
+        return "Replicate"
+
+
+class Ollama(LLMProvider):
+    def query(self, prompt, model="llama2"):
+        response = ollama.chat(
+            model=model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        return response["message"]["content"]
+
+
+class HuggingFace(LLMProvider):
+    def query(self, prompt):
+        return "HuggingFace"
