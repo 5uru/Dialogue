0449ef6c1221345e715aa85ac6044ee28bdaf42c
Jonathan Suru <jonathansuru@icloud.com>
2024-03-03 19:10:30 +0100

    🚀 feat: add function to split documents into chunks and index them in a collection

---
+++ dialogue/index_data.py
@@ -0,0 +1,83 @@
+import uuid
+from typing import Optional, List
+
+import chromadb
+from chromadb.utils import embedding_functions
+from langchain.docstore.document import Document as LangchainDocument
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from transformers import AutoTokenizer
+
+EMBEDDING_MODEL_NAME = "thenlper/gte-small"
+
+MARKDOWN_SEPARATORS = [
+    "\n#{1,6} ",
+    "```\n",
+    "\n\\*\\*\\*+\n",
+    "\n---+\n",
+    "\n___+\n",
+    "\n\n",
+    "\n",
+    " ",
+    "",
+]
+
+
+def split_documents(
+    chunk_size: int,
+    knowledge_base: List[LangchainDocument],
+    tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME,
+) -> List[LangchainDocument]:
+    # sourcery skip: inline-immediately-returned-variable
+    """
+    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
+    Args:
+        chunk_size (int): The maximum size of each chunk in tokens.
+        knowledge_base (List[LangchainDocument]): The list of documents to be split.
+        tokenizer_name(Optional[str], optional): The name of the tokenizer to use. Defaults to EMBEDDING_MODEL_NAME.
+    Returns: List[LangchainDocument]: The list of split documents.
+    """
+    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        AutoTokenizer.from_pretrained(tokenizer_name),
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_size // 10,
+        add_start_index=True,
+        strip_whitespace=True,
+        separators=MARKDOWN_SEPARATORS,
+    )
+    # Split documents
+    docs_processed = [
+        split_doc
+        for doc in knowledge_base
+        for split_doc in text_splitter.split_documents([doc])
+    ]
+    # Remove duplicates
+    unique_texts = set()
+    docs_processed_unique = [
+        doc
+        for doc in docs_processed
+        if not (doc.page_content in unique_texts or unique_texts.add(doc.page_content))
+    ]
+    return docs_processed_unique
+
+
+def indexer(docs: List[LangchainDocument], collection_name: str):
+    """
+    Add documents to the collection.
+    Args:
+        docs (List[LangchainDocument]): The list of documents to be added to the collection.
+        collection_name (str): The name of the collection.
+    """
+    client = chromadb.PersistentClient()
+    sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+        model_name=EMBEDDING_MODEL_NAME
+    )
+    collection = client.get_or_create_collection(
+        name=collection_name, embedding_function=sentence_transformer_ef
+    )
+
+    # Add documents to the collection
+    collection.add(
+        ids=[str(uuid.uuid1()) for _ in docs],
+        metadatas=[doc.metadata for doc in docs],
+        documents=[doc.page_content for doc in docs],
+    )
